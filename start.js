4942dcae3e15ef0b72c24ad8d3142dbe453c6b53
Dan Voss <dan.voss@gmail.com>
2015-04-29 10:56:46 -0400

    making start script work with Heroku

---
+++ start.js
@@ -0,0 +1,6 @@
+var app = require('./app');
+//Read the port from the environment object
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
