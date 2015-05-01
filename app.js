5067516a745cb4dddd7c108ede51ad90657e568c
Dan Voss <dan.voss@gmail.com>
2015-05-01 11:07:05 -0400

    replace in memory store with REDIS store

--- app.js
+++ app.js
@@ -2,31 +2,50 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var redis = require('redis');
+var redisClient;
 
-var cities = {
-	'Lotopia': 'cool place',
-	'Caspiana': 'nice beaches',
-	'Indigo': 'bluish'
+//Need to do a check to see if we're in prod and need to auth to REDIS DB
+//Only the prod environment has the REDISTOGO_URL environment variable set.
+//This was set when we installed RedisTogo to our Heroku instance.
+if (process.env.REDISTOGO_URL) {
+	var rtg = require('url').parse(process.env.REDISTOGO_URL);
+	var redisClient = redis.createClient(rtg.port, rtg.hostname);
+	redisClient.auth(rtg.auth.split(":")[1]);
+} else {
+	redisClient = redis.createClient();
 }
+ 
+
+var redisInstance = (process.env.NODE_ENV) ? process.env.NODE_ENV.length : 'development'.length;
+
+redisClient.select(redisInstance);
 
 app.use(express.static('public'));
 
-app.get('/', function(request, response) {
-	response.send('OK');
-});
-
 app.get('/cities', function(request, response) {
-	//var citiesList = [];
-	//for (var key in cities) {
-	//	citiesList.push(key);
-	//}
-	response.json(Object.keys(cities));
+	redisClient.hkeys('cities', function(error, cityNames) {
+		if (error) {
+			console.log('error getting cities from redis');
+		} else {
+			console.log('Number of cities: ' + cityNames.length);
+			response.json(cityNames);
+		}
+	})
 });
 
 app.post('/cities', parseUrlencoded, function(request, response) {
 	var newCity = request.body;
-	cities[newCity.name] = newCity.description;
-	response.status(201).json(newCity.name);
+
+	redisClient.hset('cities', newCity.name, newCity.description, function(error) {
+		if (error) {
+			console.log('Error saving city');
+		} else {
+			console.log('successfully saved city: ' + newCity.name);
+			response.status(201).json(newCity.name);
+		}
+	});
+
 });
 
 module.exports = app;
--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var request = require('supertest');
 var app = require('./app');
 
+var redis = require('redis');
+var redisClient = redis.createClient();
+redisClient.select('test'.length);
+redisClient.flushdb();
+
 //Use Mocha for more semantic tests
 describe('Requests to the root path', function() {
 
@@ -41,12 +46,17 @@ describe('Listing cities on /cities', function() {
 	it('Returns initial cities', function(done) {
 		request(app)
 			.get('/cities')
-			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+			.expect(JSON.stringify([]), done);
 	});
 
 });
 
 describe('Creating new cities', function() {
+
+	//Reset the DB data between each test that updates data.
+	before(function() {
+		//Or not ;-)
+	});
 
 	it('Returns a 201 status code', function(done) {
 		request(app)
@@ -59,7 +69,7 @@ describe('Creating new cities', function() {
 	it('Returns the city name', function(done) {
 		request(app)
 			.post('/cities')
-			.send('name=springfield&description=where+the+Simpsons+live')
+			.send('name=Springfield&description=where+the+Simpsons+live')
 			.expect(/springfield/i, done);
 	});
 
